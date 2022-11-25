fc2d2004d867bdf981d231363be1279d070ccf14
Dongsheng Cai <d@tux.im>
2022-11-25 15:26:57 +1100

    crkbd: keymap and build

---
+++ keyboards/crkbd/keymaps/dcai/rules.mk
@@ -0,0 +1,13 @@
+MOUSEKEY_ENABLE     = yes     # Mouse keys
+RGBLIGHT_ENABLE     = yes     # Enable WS2812 RGB underlight.
+RGB_MATRIX_ENABLE   = no      # per key RGB
+OLED_ENABLE         = yes
+OLED_DRIVER         = SSD1306
+EXTRAKEY_ENABLE     = yes
+LTO_ENABLE          = yes
+COMBO_ENABLE        = yes
+# vial
+VIA_ENABLE          = no     # Enable VIA
+VIAL_ENABLE         = no     # Enable VIAL
+VIAL_INSECURE       = yes
+QMK_SETTINGS        = no
